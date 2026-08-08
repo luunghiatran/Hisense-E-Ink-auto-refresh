8e825321004f402ad1d621b6e17694ce03185193
Liziwa <chenxu.mail@icloud.com>
2026-08-08 11:01:37 +0800

    v1.3.1 修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.liziwa.hisense_autorefresh"
         minSdk = 28
         targetSdk = 30
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
